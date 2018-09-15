e83a34706c962e0cc9dde4733ba42c92576a51ee
thetriplet3 <thetriplet3@gmail.com>
2018-09-15 20:20:12 +0530

    Changed the app server ip

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -13,20 +13,20 @@ export class RequestService {
   constructor(private http: HttpClient) { }
 
   getRequests() {
-    return this.http.get<Request>('https://localhost:44355/api/Requests');
+    return this.http.get<Request>('https://13.229.139.61:5000/api/Requests');
   }
   getRequestTypes() {
-    return this.http.get<RequestType>('https://localhost:44355/api/RequestTypes');
+    return this.http.get<RequestType>('https://13.229.139.61:5000/api/RequestTypes');
   }
   getRequest(id) {
-    return this.http.get<Request>(`https://localhost:44355/api/Requests/${id}`);
+    return this.http.get<Request>(`https://13.229.139.61:5000/api/Requests/${id}`);
   }
 
   createRequest(requestObj) {
     
-    return this.http.post<Request>(`https://localhost:44355/api/Requests/`, requestObj);
+    return this.http.post<Request>(`https://13.229.139.61:5000/api/Requests/`, requestObj);
   }
   updateRequest(requestObj) {
-    return this.http.put<Request>(`https://localhost:44355/api/Requests/${requestObj.requestId}`, requestObj);
+    return this.http.put<Request>(`https://13.229.139.61:5000/api/Requests/${requestObj.requestId}`, requestObj);
   }
 }
